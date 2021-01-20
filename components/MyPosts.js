c9ab61cb71648d09783c423301976b12c3c4cf3c
EdonJung <2donny@naver.com>
2021-01-20 13:28:22 +0900

    Responsive web page using grid

--- components/MyPosts.js
+++ components/MyPosts.js
@@ -2,8 +2,30 @@ import PostPreview from './post-preview'
 
 export default function MyPosts({ posts }) {
   return (
+    // 포스트 전체를 담는 컨테이너
     <section>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 md:gap-y-32 lg:gap-x-32 lg:gap-y-10 gap-y-10  mb-32 xl:w-3/4 float-none lg:float-right xl:float-right ">
+      <div 
+      className="
+      grid 
+      grid-cols-1 
+      md:grid-cols-2 
+      lg:grid-cols-2
+      xl:grid-cols-3 
+      gap-x-10
+      gap-y-10  
+
+      md:gap-x-8 
+      md:gap-y-12 
+      lg:gap-y-12 
+      
+      sm:w-full
+      lg:w-3/4 
+      xl:w-3/4 
+
+      float-none 
+      lg:float-right 
+      xl:float-right 
+      mb-32">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
